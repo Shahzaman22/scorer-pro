a533dfca7820fbc7d5a8553c6424d27f5daae119
[Shahzaman] <shahzaman.aftab@gmail.com>
2023-01-13 16:29:58 +0500

    Second commit

--- validators/db.js
+++ validators/db.js
@@ -7,11 +7,3 @@ async function main(){
     console.log('MongoDB connected')
 }
 
-
-// const mongoose = require('mongoose');
-//   mongoose.connect(process.env.MONGO_DB_URL)
-//   const db = mongoose.connection;
-//   db.on('error', error => console.error(error))
-//   db.once('open', () => {
-//       console.log('Connected to MongoDB')
-//   })
